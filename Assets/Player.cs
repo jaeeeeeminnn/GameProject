41931f4535b26137064986a1c9f67b7e3fe988b0
jaeeeeeminnn <jj5488jj@naver.com>
2022-08-24 14:06:47 +0900

    1단 점프 구현

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float moveX, moveY;
+    float moveX;
+
+    Rigidbody2D rb;
 
     [Header("이동속도 조절")]
-    [SerializeField] [Range(1f, 30f)] float moveSpeed = 20f;
+    [SerializeField] [Range(100f, 800f)] float moveSpeed = 200f;
 
+    [Header("점프 강도")]
+    [SerializeField] [Range(100f, 800f)] float jumpForce = 400f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
         //이동
         moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        rb.velocity = new Vector2(moveX, rb.velocity.y);
 
-        transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (rb.velocity.y == 0)
+            {
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+            }
+        }
+
+
     }
 }
